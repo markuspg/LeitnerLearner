caac003f0bc907a15d5ee78728b2b132f6b87005
markuspg <markuspg@users.noreply.github.com>
2019-09-14 16:57:44 +0200

    Add savedVerses property to Backend class

--- src/backend.h
+++ src/backend.h
@@ -9,18 +9,24 @@ class ConfigurationHandler;
 class Backend : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(int savedVerses READ GetSavedVerses() NOTIFY SavedVersesChanged)
 
 public:
     Backend(QObject *argParent = nullptr);
 
     ConfigurationHandler* GetConfigHndlr() const noexcept { return configHndlr; }
+    int GetSavedVerses() const noexcept { return savedVersesQty; }
     AbstractStorageBackend* GetStorageBcknd() const noexcept { return storageBackend; }
     void Initialize();
     Q_INVOKABLE void saveVerse(const QString &argBook, int argChapterNo,
                                int argVerseNo, const QString &argVerseText);
 
+signals:
+    void SavedVersesChanged();
+
 private:
     ConfigurationHandler *const configHndlr = nullptr;
+    unsigned int savedVersesQty = 0u;
     AbstractStorageBackend *const storageBackend = nullptr;
 };
 
