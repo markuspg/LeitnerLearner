5ef82a32049ee29362678f425040ac8cd7cc1c88
markuspg <markuspg@users.noreply.github.com>
2019-09-14 10:51:13 +0200

    Introduce Backend class

---
+++ src/backend.h
@@ -0,0 +1,21 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include <QObject>
+
+class AbstractStorageBackend;
+class ConfigurationHandler;
+
+class Backend : public QObject
+{
+    Q_OBJECT
+
+public:
+    Backend(QObject *argParent = nullptr);
+
+private:
+    ConfigurationHandler *const configHndlr = nullptr;
+    AbstractStorageBackend *const storageBackend = nullptr;
+};
+
+#endif // BACKEND_H
